cf937459452c2b52004162dddc3571761ddce727
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2025-12-23 11:30:07 +0530

    commit

---
+++ Module1/23-12/Hello.java
@@ -0,0 +1,13 @@
+public class Hello 
+{
+	public static void main(String tops[])
+	{
+		/*
+			System.out.print("\n Hello World");
+			System.out.print("\n Tops Technologies");
+		*/
+		
+		System.out.println("Hello World");
+		System.out.println("Tops Technologies");
+	}
+}
